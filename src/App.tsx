93423760c9be8eaf8ecbb65e0f6fd2941552b2be
Konrad Mikucki <komik966@gmail.com>
2019-03-28 07:54:37 +0100

    selectable markers

--- src/App.tsx
+++ src/App.tsx
@@ -16,10 +16,12 @@ import {
 } from '@material-ui/core';
 
 function App() {
-    const [toggle, setToggle] = useState(true);
+    const [selectedPlace, setSelectedPlace] = useState<string>('');
+    const [source, setSource] = useState<string>('');
+    const [destination, setDestination] = useState<string>('');
     return (
         <Layout>
-            {toggle && (<div style={{
+            <div style={{
                 position: 'absolute',
                 zIndex: 9999,
                 top: '66px',
@@ -31,35 +33,40 @@ function App() {
                             <ListItemIcon>
                                 <PlaceIcon color='primary'/>
                             </ListItemIcon>
-                            <ListItemText inset primary="AGH \ UR" secondary="Skąd"/>
+                            <ListItemText inset primary={source} secondary="Skąd"/>
                         </ListItem>
-                        <Divider />
+                        <Divider/>
                         <ListItem button>
                             <ListItemIcon>
                                 <PlaceIcon color='secondary'/>
                             </ListItemIcon>
-                            <ListItemText inset primary="Czarnowiejska" secondary="Dokąd" />
+                            <ListItemText inset primary={destination} secondary="Dokąd"/>
                         </ListItem>
-                        <Divider />
+                        <Divider/>
                         <ListItem button>
-                            <ListItemText inset primary="10:00 30.03.2019" secondary="Data odjazdu" style={{padding: 0}}/>
+                            <ListItemText inset primary="10:00 30.03.2019" secondary="Data odjazdu"
+                                          style={{padding: 0}}/>
                         </ListItem>
                     </List>
-                    <Divider />
-                    <CardHeader title="Czarnowiejska" style={{padding: '16px 16px 8px 8px'}}/>
-                    <CardActions>
-                        <Button size='small' color='primary' variant='outlined'>
-                            Start
-                        </Button>
-                        <Button size='small' color='secondary' variant='outlined'>
-                            Cel
-                        </Button>
-                    </CardActions>
+                    <Divider/>
+                    {selectedPlace &&
+                    (<>
+                        <CardHeader title={selectedPlace} style={{padding: '16px 16px 8px 8px'}}/>
+                        <CardActions>
+                            <Button size='small' color='primary' variant='outlined' onClick={() => setSource(selectedPlace)}>
+                                Start
+                            </Button>
+                            <Button size='small' color='secondary' variant='outlined' onClick={() => setDestination(selectedPlace)}>
+                                Cel
+                            </Button>
+                        </CardActions>
+                    </>)
+                    }
                 </Card>
-            </div>)}
-            <Map center={[50.062037, 19.937735]} zoom={14}>
-                <Marker position={[50.062037, 19.937735]}/>
-                <Marker position={[50.062037, 19.927735]} onClick={() => setToggle(!toggle)}/>
+            </div>
+            <Map center={[50.062037, 19.937735]} zoom={14} onClick={() => setSelectedPlace('')}>
+                <Marker position={[50.062037, 19.937735]} onClick={() => setSelectedPlace('AGH \\ UR')}/>
+                <Marker position={[50.062037, 19.927735]} onClick={() => setSelectedPlace('Czarnowiejska')}/>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
                     url='https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png'
