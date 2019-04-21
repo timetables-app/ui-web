d497f96525a4f62414aad3d52e5a1808ac039fd7
Konrad Mikucki <komik966@gmail.com>
2019-04-21 09:31:46 +0200

    ReduxForm bridge text field

--- src/ReduxFormBridge/TextField.tsx
+++ src/ReduxFormBridge/TextField.tsx
@@ -1,34 +1,31 @@
 import { TextField as MuiTextField } from '@material-ui/core';
 // tslint:disable-next-line:no-submodule-imports
 import { TextFieldProps } from '@material-ui/core/TextField';
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, ReactNode } from 'react';
 import { Field, WrappedFieldProps } from 'redux-form';
 
-const TextField: FunctionComponent<Props> = props => {
-  const { rows, value, defaultValue, variant, ...rest } = props;
-  return (
-    <Field
-      component={renderTextField({ rows, value, defaultValue, variant })}
-      {...rest}
-    />
-  );
+const TextField: FunctionComponent<Props> = ({
+  name,
+  ...muiTextFieldProps
+}) => {
+  return <Field name={name} component={render(muiTextFieldProps)} />;
 };
 
 type Props = TextFieldProps & {
   name: string;
 };
 
-const renderTextField = (muiPropsIncompatibleWithReduxForm: any) => ({
+const render: (
+  muiTextFieldProps: TextFieldProps
+) => (reduxFormProps: WrappedFieldProps) => ReactNode = muiTextFieldProps => ({
   input,
-  meta: { touched, invalid, error },
-  ...custom
-}: WrappedFieldProps) => (
+  meta: { touched, invalid, error }
+}) => (
   <MuiTextField
     error={touched && invalid}
     helperText={touched && error}
     {...input}
-    {...custom}
-    {...muiPropsIncompatibleWithReduxForm}
+    {...muiTextFieldProps}
   />
 );
 
